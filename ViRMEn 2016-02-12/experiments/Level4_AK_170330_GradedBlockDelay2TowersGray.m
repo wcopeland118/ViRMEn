b121541ad629e265995a430ccdd83d7290080326
atk <aaron.t.kuan@gmail.com>
2017-05-02 14:09:29 -0400

    set length of graded block delay back to 400

--- ViRMEn 2016-02-12/experiments/Level4_AK_170330_GradedBlockDelay2TowersGray.m
+++ ViRMEn 2016-02-12/experiments/Level4_AK_170330_GradedBlockDelay2TowersGray.m
@@ -14,10 +14,10 @@ code.termination = @terminationCodeFun;
 % --- INITIALIZATION code: executes before the ViRMEN engine starts.
 function vr = initializationCodeFun(vr)
 
-vr.debugMode = true;
+vr.debugMode = false;
 
-vr.mouseNum = 999;
-vr.greyFac = 0/6; %goes from 0 to 1 to signify the amount of maze which is grey
+vr.mouseNum = 002;
+vr.greyFac = 3/6; %goes from 0 to 1 to signify the amount of maze which is grey
 vr.maxGrey = 3/6;
 vr.blockThresh = [0 1/6 2/6 3/6 4/6]; %thresholds for blocks
 vr.trialBlockThresh = 20;
